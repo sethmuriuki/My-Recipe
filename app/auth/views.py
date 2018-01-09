6cf7e4ad152d2ed2679a03a98e6530110a41d956
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-09 16:00:19 +0300

    add log out function

--- app/auth/views.py
+++ app/auth/views.py
@@ -33,3 +33,10 @@ def register():
 
     title = "One Minute Pitch Login"
     return render_template('auth/register.html',registration_form = form,title = title)
+
+#logout
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
