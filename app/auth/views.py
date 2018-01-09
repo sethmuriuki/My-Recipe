7bc60d38e2988478af06f49354275340102ba686
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-09 15:57:14 +0300

    add dynamic routes for registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -20,15 +20,16 @@ def login():
     return render_template('auth/login.html', login_form=login_form)
 
 #registration routes
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(email=login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
-            login_user(user, login_form.remember.data)
-            return redirect(url_for('main.index'))
+@auth.route('/register',methods = ['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next')or url_for('main.index'))
 
-        flash('Invalid username or Password')
+        flash('invalid password')
 
-    return render_template('auth/login.html', login_form=login_form)
+    title = "One Minute Pitch Login"
+    return render_template('auth/register.html',registration_form = form,title = title)
