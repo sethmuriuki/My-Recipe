45a1d113471f684f8c881aa811ca6ddb09c1ce97
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-10 09:12:39 +0300

    create config file

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+class Config:
+    '''
+    General configurations parent class 
+    '''
+
+    RECIPE_BASE_URL = "http://www.themealdb.com/api/json/v1/1/lookup.php?i=52772"
+
+
+class ProdConfig(Config):
+    pass
+
+
+
+class DevConfig(Config):
+   
+   DEBUG = True
+   
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
