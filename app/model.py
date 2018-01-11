c36b36139ee66d3d748dea887d4e9cbcb0fe1563
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-11 09:21:09 +0300

    create user class to define new users

--- app/model.py
+++ app/model.py
@@ -7,3 +7,20 @@ from datetime import datetime
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+#users
+class User(UserMixin,db.Model):
+    '''
+    user class to create new users
+    '''
+    __tablename__ = 'users'
+
+    #addidng columns
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique=True,index=True)
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    pitches = db.relationship("Talks", backref="user", lazy = "dynamic")
+    comment = db.relationship("Comments", backref="user", lazy = "dynamic")
+    
