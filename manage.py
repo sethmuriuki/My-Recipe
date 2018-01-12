70892deea779826ec19f3103b7b7152af92a3135
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-12 15:54:16 +0300

    deploy to heroku

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from  flask_migrate import Migrate, MigrateCommand
 from flask_login import LoginManager
 
 # Creating app instances
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
